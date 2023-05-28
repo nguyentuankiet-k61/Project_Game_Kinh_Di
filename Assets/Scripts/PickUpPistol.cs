5bd06a9142fc62a699838c3e6fb87d3aa0d8ba3d
nguyentuankiet-k61 <kietnguyen031296@gmail.com>
2023-05-28 21:19:18 +0700

    Update Let the player hold the gun and do Animation when shooting

---
+++ Assets/Scripts/PickUpPistol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpPistol : MonoBehaviour
+{
+    public float theDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject FakePistol;
+    public GameObject RealPistol;
+    public GameObject GuideArrow;
+    void Update()
+    {
+        theDistance = PlayerCasting.DistanceFromTarget;
+
+    }
+    void OnMouseOver()
+    {
+        if (theDistance < 3)
+        {
+            ActionText.GetComponent<Text>().text = "Hãy lấy súng";
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+        if (Input.GetButtonDown("Action"))
+        {
+            if (theDistance < 3)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                FakePistol.SetActive(false);
+                RealPistol.SetActive(true);
+                GuideArrow.SetActive(false);
+            }
+
+        }
+    }
+    void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
